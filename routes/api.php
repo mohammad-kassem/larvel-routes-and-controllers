17ca881853741003207f451668e022c0cedc8618
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-06 17:41:19 +0300

    implementing the TeamController with the generateTeams function PHP

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\PalindromeController;
 use App\Http\Controllers\TimeController;
 use App\Http\Controllers\DisplayTextController;
 use App\Http\Controllers\BeerRecipeController;
+use App\Http\Controllers\TeamsController;
+
 
 
 
@@ -15,3 +17,4 @@ Route::get('/count-palindromes', [PalindromeController::class, 'countPalindromes
 Route::get('/count-seconds', [TimeController::class, 'countSeconds'])->name("count-seconds");
 Route::get('/display-text', [DisplayTextController::class, 'displayText'])->name("display-text");
 Route::get('/beer-recipe', [BeerRecipeController::class, 'getRandomRecipe'])->name("random-recipe");
+Route::get('/generate-teams', [TeamsController::class, 'generateTeams'])->name("genrate-teams");
