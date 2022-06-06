833aa3ab9b5889eab436c6575e0418b5312e8849
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-06 18:22:31 +0300

    minor fixes

--- app/Http/Controllers/BeerRecipeController.php
+++ app/Http/Controllers/BeerRecipeController.php
@@ -9,7 +9,7 @@ class BeerRecipeController extends Controller{
     public function getRandomRecipe(){
         $beer_recipes_array = Http::get('https://api.punkapi.com/v2/beers');
         $n = sizeof(json_decode($beer_recipes_array));
-        $random_index = rand(1, $n);
+        $random_index = rand(0, $n-1);
         return response()->json([
             "status" => "Success",
             "beer-recipe" => $beer_recipes_array[$random_index]
