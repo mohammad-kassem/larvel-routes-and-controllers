97dd492d92c8011d305dcef9f4178f511beabb03
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-06 16:19:51 +0300

    implementing teh BeerRecipeController with the getRandomRecipe function PHP

---
+++ app/Http/Controllers/BeerRecipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+
+class BeerRecipeController extends Controller{
+    public function getRandomRecipe(){
+        $beer_recipes_array = Http::get('https://api.punkapi.com/v2/beers');
+        $n = sizeof(json_decode($beer_recipes_array));
+        $random_index = rand(1, $n);
+        return response()->json([
+            "status" => "Success",
+            "beer-recipe" => $beer_recipes_array[$random_index]
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PalindromeController;
 use App\Http\Controllers\TimeController;
 use App\Http\Controllers\DisplayTextController;
+use App\Http\Controllers\BeerRecipeController;
+
 
 
 
@@ -12,3 +14,4 @@ use App\Http\Controllers\DisplayTextController;
 Route::get('/count-palindromes', [PalindromeController::class, 'countPalindromes'])->name("count-palindromes");
 Route::get('/count-seconds', [TimeController::class, 'countSeconds'])->name("count-seconds");
 Route::get('/display-text', [DisplayTextController::class, 'displayText'])->name("display-text");
+Route::get('/beer-recipe', [BeerRecipeController::class, 'getRandomRecipe'])->name("random-recipe");
