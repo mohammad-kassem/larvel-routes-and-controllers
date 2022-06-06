9ee877479c4c99a387d3c1ded45bd02ab304cdc3
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-06 17:53:05 +0300

    fixing the TeamscController generateTeams function PHP

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -5,32 +5,9 @@ use Illuminate\Http\Request;
 
 class TeamsController extends Controller{
     public function generateTeams(){
-        $students_array = ['sara', 'ali', 'joe', 'mariam', 'pablo', "iman"];
+        $students_array = ['sara', 'ali', 'joe', 'mariam', 'pablo'];
         $n = sizeof($students_array);
-        $team = [];
-        $teams_array = [];
-        $assigned_indices = [];
-        // $teams_counter = 0;
-        while (count($teams_array, COUNT_RECURSIVE)  < $n){
-            $random_student = rand(0, $n - 1);
-            if (!in_array($random_student, $assigned_indices)){
-                array_push($team, $students_array[$random_student]);
-                // echo json_encode(sizeof($teams_array,1));
-                array_push($assigned_indices, $random_student);
-                if (sizeof($team) === 2){
-                    array_push($teams_array, $team);
-                    $team = [];
-                // if (sizeof($assigned_indices) % 2 == 0){
-                //     $teams_counter++;
-                // }
-            }
-
-        }
-    }
-        
-        if ($team !== []){
-            array_push($teams_array, $team); 
-        }
+        $teams_array = array_chunk($students_array, 2);
         return response()->json([
             "status" => "Success",
             "teams" => $teams_array
