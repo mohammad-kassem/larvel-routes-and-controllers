4357cf81230781f5da2d0708b3ced54628d46af6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-06 15:05:07 +0300

    implementing the TimeController with the countSeconds function PHP

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PalindromeController;
+use App\Http\Controllers\TimeController;
+
 
 
 Route::get('/count-palindromes', [PalindromeController::class, 'countPalindromes'])->name("count-palindromes");
+Route::get('/count-seconds', [TimeController::class, 'countSeconds'])->name("count-seconds");
