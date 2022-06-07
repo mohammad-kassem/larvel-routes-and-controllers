ff80f4e3cd3b874f4a4dbe8fcad5e1a7225fa495
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-07 16:17:58 +0300

    updating the routes to the controller changes

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PalindromeController;
-use App\Http\Controllers\TimeController;
-use App\Http\Controllers\DisplayTextController;
-use App\Http\Controllers\BeerRecipeController;
-use App\Http\Controllers\TeamsController;
-use App\Http\Controllers\NominationController;
+use App\Http\Controllers\AssignmentController;
 
 
 
@@ -15,9 +10,9 @@ use App\Http\Controllers\NominationController;
 
 
 
-Route::get('/count-palindromes', [PalindromeController::class, 'countPalindromes'])->name("count-palindromes");
-Route::get('/count-seconds', [TimeController::class, 'countSeconds'])->name("count-seconds");
-Route::get('/display-text', [DisplayTextController::class, 'displayText'])->name("display-text");
-Route::get('/beer-recipe', [BeerRecipeController::class, 'getRandomRecipe'])->name("random-recipe");
-Route::get('/generate-teams', [TeamsController::class, 'generateTeams'])->name("genrate-teams");
-Route::get('/generate-nomination', [NominationController::class, 'generateNomination'])->name("genrate-nomination");
+Route::get('/palindromes_count', [AssignmentController::class, 'countPalindromes'])->name("palindromes_count");
+Route::get('/seconds', [AssignmentController::class, 'countSeconds'])->name("seconds");
+Route::get('/joke', [AssignmentController::class, 'displayText'])->name("joke");
+Route::get('/beer_recipe', [AssignmentController::class, 'getRandomRecipe'])->name("beer_recipe");
+Route::get('/teams', [AssignmentController::class, 'generateTeams'])->name("teams");
+Route::get('/nomination', [AssignmentController::class, 'generateNomination'])->name("nomination");
